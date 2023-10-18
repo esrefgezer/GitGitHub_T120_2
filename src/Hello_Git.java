16c093db5a237285f9bf088a8157ea955087195f
Esref Gezer <esrefgezer72@gmail.com>
2023-10-18 17:33:35 +0200

    bakalim

--- src/Hello_Git.java
+++ src/Hello_Git.java
@@ -5,6 +5,7 @@ public class Hello_Git {
 
         //git add sadece content root path ile adresindeki dosyayi gonderir
         //git add . Tum dosyalari stage area atar
+        System.out.println("anlamadim");
 
     }
 }
