1bfb3221ecef52472fee27dd1ebcc19fe2c1cfb6
Esref Gezer <esrefgezer72@gmail.com>
2023-10-18 17:50:18 +0200

    first commit

--- src/gitHubDay02_2.java
+++ src/gitHubDay02_2.java
@@ -1,5 +1,8 @@
 public class gitHubDay02_2 {
     public static void main(String[] args) {
         System.out.println("Ikinci dosya birinci satir");
+
+        System.out.println("Oldu mu?");
+
     }
 }
