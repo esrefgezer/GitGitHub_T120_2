640cf9ccd7363cfd7ff1f4f270e081e9a9ff11af
Esref Gezer <esrefgezer72@gmail.com>
2023-10-18 17:31:00 +0200

    gormek icin yazdim

--- src/gitHubday02_1.java
+++ src/gitHubday02_1.java
@@ -2,5 +2,11 @@ public class gitHubday02_1 {
     public static void main(String[] args) {
         System.out.println("Merhaba tekradan gitHub");
 
+        //git init ile bagla
+        //git add .
+        //git commit -m" yamak istedigini yaz"
+        //git push ile gonder
+        System.out.println("bunu ogrendin mi?");
+
     }
 }
